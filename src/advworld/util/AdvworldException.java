5e71b11d761622f2ad31ad1fded3b122ce6b77b7
mountain7cat <mountain7cat@34766076-6df9-11de-b181-cfa5ed37d7b3>
2009-07-20 21:32:39 +0000

    Exception class for the project. Universal exception class.

---
+++ src/advworld/util/AdvworldException.java
@@ -0,0 +1,12 @@
+package advworld.util;
+
+public class AdvworldException extends Exception {
+
+	public AdvworldException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public AdvworldException(String arg0) {
+		super(arg0);
+	}
+}
